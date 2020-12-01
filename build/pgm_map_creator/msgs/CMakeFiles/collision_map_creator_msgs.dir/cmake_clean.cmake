72c440224256977a5069585f68e53fa61aa348dd
ChenXuOffWorld <chen.xu@offworld.ai>
2020-12-01 00:16:46 -0800

    Update:add amcl dependency for project

--- build/pgm_map_creator/msgs/CMakeFiles/collision_map_creator_msgs.dir/cmake_clean.cmake
+++ build/pgm_map_creator/msgs/CMakeFiles/collision_map_creator_msgs.dir/cmake_clean.cmake
@@ -1,16 +1,7 @@
 file(REMOVE_RECURSE
   "collision_map_request.pb.cc"
   "collision_map_request.pb.h"
-  "vector2d.pb.cc"
-  "vector2d.pb.h"
-  "header.pb.cc"
-  "header.pb.h"
-  "time.pb.cc"
-  "time.pb.h"
   "CMakeFiles/collision_map_creator_msgs.dir/collision_map_request.pb.cc.o"
-  "CMakeFiles/collision_map_creator_msgs.dir/vector2d.pb.cc.o"
-  "CMakeFiles/collision_map_creator_msgs.dir/header.pb.cc.o"
-  "CMakeFiles/collision_map_creator_msgs.dir/time.pb.cc.o"
   "/home/offworld/Desktop/workspace/catkin_ws/devel/lib/libcollision_map_creator_msgs.pdb"
   "/home/offworld/Desktop/workspace/catkin_ws/devel/lib/libcollision_map_creator_msgs.so"
 )
