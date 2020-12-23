d347c91742a71c0fb2874f9c142af991868336f1
ChenXuOffWorld <chen.xu@offworld.ai>
2020-12-22 16:43:23 -0800

    Update:update the location of map and add teleop package

--- build/catkin_generated/generate_cached_setup.py
+++ build/catkin_generated/generate_cached_setup.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
-import argparse
+
 import os
 import stat
 import sys
@@ -12,7 +12,7 @@ try:
     from catkin.environment_cache import generate_environment_script
 except ImportError:
     # search for catkin package in all workspaces and prepend to path
-    for workspace in "/home/offworld/Desktop/workspace/catkin_ws/devel;/home/offworld/swarm-robotic-mining/catkin_ws/devel;/opt/ros/kinetic".split(';'):
+    for workspace in '/home/offworld/swarm-robotic-mining/catkin_ws/devel;/opt/ros/kinetic'.split(';'):
         python_path = os.path.join(workspace, 'lib/python2.7/dist-packages')
         if os.path.isdir(os.path.join(python_path, 'catkin')):
             sys.path.insert(0, python_path)
@@ -23,7 +23,7 @@ code = generate_environment_script('/home/offworld/Desktop/workspace/catkin_ws/d
 
 output_filename = '/home/offworld/Desktop/workspace/catkin_ws/build/catkin_generated/setup_cached.sh'
 with open(output_filename, 'w') as f:
-    #print('Generate script for cached setup "%s"' % output_filename)
+    # print('Generate script for cached setup "%s"' % output_filename)
     f.write('\n'.join(code))
 
 mode = os.stat(output_filename).st_mode
